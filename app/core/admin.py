867eadbcc461eee6c982e7b270236c8c4c85c352
Yankı Aydın <yankiaydin@gmail.com>
2021-07-21 15:33:03 +0300

    Added working list tags endpoint

--- app/core/admin.py
+++ app/core/admin.py
@@ -3,7 +3,6 @@ from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.utils.translation import gettext as _
 from core import models
 
-
 class UserAdmin(BaseUserAdmin):
     ordering = ['id']
     list_display = ['email', 'name']
@@ -31,3 +30,4 @@ class UserAdmin(BaseUserAdmin):
 
 
 admin.site.register(models.User, UserAdmin)
+admin.site.register(models.Tag)
--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
 from django.contrib.auth import get_user_model
+from core import models
 
 class ModelTest(TestCase):
     def test_create_user_with_email_successful(self):
@@ -33,3 +34,16 @@ class ModelTest(TestCase):
 
         self.assertTrue(user.is_superuser)
         self.assertTrue(user.is_staff)
+
+def sample_user(email='test@londonappdev.com', password='testpass'):
+    """Create a sample user"""
+    return get_user_model().objects.create_user(email, password)
+
+def test_tag_str(self):
+    """Test the tag string representation"""
+    tag = models.Tag.objects.create(
+        user=sample_user(),
+        name='Vegan'
+    )
+
+    self.assertEqual(str(tag), tag.name)
